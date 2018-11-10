4f126671b6c6bfe16b161a4afcc8e4ee5b518645
MrSoltys <mrsoltys@gmail.com>
2018-11-10 14:57:44 -0700

    added CO sensor code

--- nightLight.ino
+++ nightLight.ino
@@ -8,8 +8,9 @@
 //    CLOUD VARIABLES       //
 //////////////////////////////
 int lightReading = 0;
-long wakeupTime=0;
+long wakeupTime=630;
 int currentTime=0;
+int coReading=0;
 
 //////////////////////////////
 //    VARS FOR NEOPIXELS    //
@@ -22,7 +23,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, WS2812B);
 void setup(){
   pinMode(A5,INPUT);  // Photocell
   pinMode(D2, INPUT); // Temp Humidity
-  pinMode(A1, INPUT); // Methane
+  pinMode(A1, INPUT); // CO
   pinMode(D3, OUTPUT); // PWM Heater for Methan Sensor
 
   // Iniltize Neopixels
@@ -30,7 +31,7 @@ void setup(){
   strip.show();
 
   // Initilize Time (And correct for DST)
-  Time.zone(-6);
+  Time.zone(-7);
     if(Time.isDST())
         Time.beginDST();
     else
@@ -38,17 +39,18 @@ void setup(){
 
   // Initilize Cloud Variables 
   Particle.variable("lightReading", lightReading);
+  Particle.variable("coReading", coReading);
   Particle.variable("wakeupTime", wakeupTime);
   Particle.variable("currentTime", currentTime);
   Particle.function("setWakeup",setWakeup);
 }
 
 void loop(){
-	lightReading = constrain(map(analogRead(A0),1850,250,100,255),0,255);
-	currentTime=(Time.hour()+1)*100+Time.minute();
+	lightReading = constrain(map(analogRead(A5),1850,250,100,255),0,255);
+	currentTime=(Time.hour())*100+Time.minute();
+  mq07timers();
 
-	delay(500);
-	if(currentTime > wakeupTime){
+	if(currentTime > wakeupTime && currentTime< wakeupTime+200){
 		for(int i=0;i<7;i++)
 		  	strip.setPixelColor(i, 0, 255-lightReading, 0);	
 	}
@@ -62,4 +64,36 @@ void loop(){
 int setWakeup(String args){
 	wakeupTime=args.toInt();
 	return wakeupTime;
+}
+
+//////////////////////////////
+//    VARS for MQ-07 CO     //
+//////////////////////////////
+bool heaterOn=false;
+bool sensorOn=false;
+unsigned long heaterTimer=0;
+void mq07timers(){
+    //Runs CO sensor every 2 minutes. 
+    //Cycle is:
+    // off 2 minutes
+    // heater on (D3->5V) for 1 minute
+    // heater low (D3->1.4V) for 1.5 minutes
+    // take reading
+    if(heaterOn && millis()-heaterTimer>60000){
+    analogWrite(D3,1.4/5*255);
+    heaterOn=false;
+    sensorOn=true;
+    heaterTimer=millis();
+  }
+  else if(sensorOn && millis()-heaterTimer>90000){
+    coReading=analogRead(A1);
+    analogWrite(D3,0);
+    sensorOn=false;
+    heaterTimer=millis();
+  }
+  else if (millis()-heaterTimer>120000){
+    analogWrite(D3,255);
+    heaterOn=true;
+    heaterTimer=millis();
+  }
 }
